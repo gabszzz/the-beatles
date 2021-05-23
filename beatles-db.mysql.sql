2536316c78c6b0d998a13f3402a33e7258cec2ad
Gabriel Teodoro <gabriel.skywalkerr@gmail.com>
2021-05-23 15:04:06 -0300

    script sql

---
+++ beatles-db.mysql.sql
@@ -0,0 +1,12 @@
+DROP DATABASE BEATLES_DB;
+CREATE DATABASE BEATLES_DB;
+USE BEATLES_DB;
+
+-- //////////////////////// --
+
+CREATE TABLE Usuario (
+	idUsuario INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR (50),
+    login VARCHAR (50),
+    senha VARCHAR (16)
+);
